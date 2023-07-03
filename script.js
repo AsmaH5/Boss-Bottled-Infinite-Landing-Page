7b296a319f54bf94c7eae281c745ff2bf78e80b9
Asma Hassan <asmasheikhhassan@gmail.com>
2023-07-03 01:26:03 +0100

    Added css styling to jumbotron and made the header stick to the top

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const closeButton = document.getElementById("closeButton");
 closeButton.addEventListener("click", function() {
   closeButton.parentNode.remove();
 });
+
+function toggleVideo() {
+  var video = document.getElementById("banner-video");
+  var button = document.getElementById("video-toggle");
+
+  if (video.paused) {
+    video.play();
+    button.innerHTML = "Pause";
+  } else {
+    video.pause();
+    button.innerHTML = "Play";
+  }
+}
+
+const video = document.getElementById('banner-video');
+const playPauseButton = document.getElementById('video-toggle');
+
+playPauseButton.addEventListener('click', toggleVideo);
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 1040) {
+    video.pause();
+    playPauseButton.innerHTML = 'Play';
+  } else {
+    video.play();
+    playPauseButton.innerHTML = 'Pause';
+  }
+});
+
+window.addEventListener('load', function() {
+  if (window.innerWidth > 1040) {
+    video.play();
+    playPauseButton.innerHTML = 'Pause';
+  }
+});
+
+
